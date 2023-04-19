486c0e9664e95b1d89d234023647d4c4ec725dc2
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-04-19 14:45:46 +0200

    add some changes

--- script.js
+++ script.js
@@ -17,29 +17,29 @@ if(typeof lname !== 'undefined' && lname !== null){
    document.getElementById('textInput1').value = '';
 }
 
-function printText() {
+function printText(dept) {
    const textInput = document.getElementById("textInput").value;
    const textInput1 = document.getElementById("textInput1").value;
    const canvas = document.createElement("canvas");
    var dwn = document.createElement('a');
    const ctx = canvas.getContext("2d");
-   const img = new Image();
-   var dept = document.getElementById('dept').value;
+   const img = document.getElementById('image');
+   // var dept = document.getElementById('dept').value;
    switch (dept){
       case "A":
-         var background = 'a.jpg';
+         var background = 'Screenshot 2023-04-18 143809.png';
          var textcolor = '#7E8080';
          var arabicfont = '200px arabic';
          var englishfont = '200px english';
          break;
       case 'B':
-         var background = 'b.jpg';
+         var background = 'Screenshot 2023-04-18 143809.png';
          var textcolor = '#00007A';
          var arabicfont = '200px arabic';
          var englishfont = '200px english';
          break;
       case "C":
-         var background = 'c.jpg';
+         var background = 'Screenshot 2023-04-18 143809.png';
          var textcolor = '#4AC1E0';
          var arabicfont = '200px arabicmts';
          var englishfont = '200px englishmts';
@@ -48,19 +48,19 @@ function printText() {
    }
 
     //img.src = "bg.jpg";
-   img.src = background;
+   img.setAttribute('src', background);
    img.onload = function() {
-     canvas.width = img.width;
-     canvas.height = img.height;
-     ctx.drawImage(img, 0, 0);
+   //   canvas.width = img.width;
+   //   canvas.height = img.height;
+   //   ctx.drawImage(img, 0, 0);
      ctx.fillStyle = textcolor;
      ctx.textAlign = "center";
      ctx.font = arabicfont;
      ctx.fillText(textInput, canvas.width/2, (canvas.height/2)+2200);
      ctx.font = englishfont;
      ctx.fillText(textInput1, canvas.width/2, (canvas.height/2)+2500);
-     dwnimg = document.getElementById("image").src = canvas.toDataURL();
-     test = dwnimg;
+   //   dwnimg = document.getElementById("image").src = canvas.toDataURL();
+   //   test = dwnimg;
    }
    document.getElementById('download').style.display = 'inline';
 }
