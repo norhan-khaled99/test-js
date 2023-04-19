0c17f65804425b9e9888d49318caec4cab977855
Mario-E-G <marioehab330@gmail.com>
2023-04-19 22:10:31 +0200

    fixing issues

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-function get(name){
-  if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-     return decodeURIComponent(name[1]);
+function get(name) {
+   if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
 }
 document.getElementById('download').style.display = 'none';
 var fname = get('fname');
 var lname = get('lname');
-if(typeof fname !== 'undefined' && fname !== null){
-document.getElementById('textInput').value = fname;
-//document.getElementById('textInput1').value = lname;
-}else{
+if (typeof fname !== 'undefined' && fname !== null) {
+   document.getElementById('textInput').value = fname;
+   //document.getElementById('textInput1').value = lname;
+} else {
    document.getElementById('textInput').value = '';
 }
-if(typeof lname !== 'undefined' && lname !== null){
+if (typeof lname !== 'undefined' && lname !== null) {
    document.getElementById('textInput1').value = lname;
-}else{
+} else {
    document.getElementById('textInput1').value = '';
 }
 
-function printText(dept) {
+function printText() {
    const textInput = document.getElementById("textInput").value;
    const textInput1 = document.getElementById("textInput1").value;
    const canvas = document.createElement("canvas");
    var dwn = document.createElement('a');
    const ctx = canvas.getContext("2d");
    const img = document.getElementById('image');
-   // var dept = document.getElementById('dept').value;
-   switch (dept){
+   var dept = document.getElementById('dept').value;
+   switch (dept) {
       case "A":
          var background = 'Screenshot 2023-04-18 143809.png';
          var textcolor = '#7E8080';
@@ -33,39 +33,40 @@ function printText(dept) {
          var englishfont = '200px english';
          break;
       case 'B':
-         var background = 'Screenshot 2023-04-18 143809.png';
+         var background = 'Screenshot_20230110_043030.png';
          var textcolor = '#00007A';
          var arabicfont = '200px arabic';
          var englishfont = '200px english';
          break;
       case "C":
-         var background = 'Screenshot 2023-04-18 143809.png';
+         var background = 'Screenshot_20230110_041431.png';
          var textcolor = '#4AC1E0';
          var arabicfont = '200px arabicmts';
          var englishfont = '200px englishmts';
          break;
-      
    }
 
-    //img.src = "bg.jpg";
+   //img.src = "bg.jpg";
    img.setAttribute('src', background);
-   img.onload = function() {
-   //   canvas.width = img.width;
-   //   canvas.height = img.height;
-   //   ctx.drawImage(img, 0, 0);
-     ctx.fillStyle = textcolor;
-     ctx.textAlign = "center";
-     ctx.font = arabicfont;
-     ctx.fillText(textInput, canvas.width/2, (canvas.height/2)+2200);
-     ctx.font = englishfont;
-     ctx.fillText(textInput1, canvas.width/2, (canvas.height/2)+2500);
-   //   dwnimg = document.getElementById("image").src = canvas.toDataURL();
-   //   test = dwnimg;
+   console.log(img.getAttribute('src'));
+
+   img.onload = function () {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      ctx.fillStyle = textcolor;
+      ctx.textAlign = "center";
+      ctx.font = arabicfont;
+      ctx.fillText(textInput, canvas.width / 2, (canvas.height / 2) + 2200);
+      ctx.font = englishfont;
+      ctx.fillText(textInput1, canvas.width / 2, (canvas.height / 2) + 2500);
+      dwnimg = document.getElementById("image").src = canvas.toDataURL();
+      test = dwnimg;
    }
    document.getElementById('download').style.display = 'inline';
 }
 
-function download(){
+function download() {
    printText();
    //var downloadlink = document.createElement()
    setTimeout(function () {
