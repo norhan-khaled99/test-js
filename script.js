fc3eb582b578d6b32130730866bd3cce909d8e77
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-04-19 12:42:02 +0200

    add files

---
+++ script.js
@@ -0,0 +1,77 @@
+function get(name){
+  if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+     return decodeURIComponent(name[1]);
+}
+document.getElementById('download').style.display = 'none';
+var fname = get('fname');
+var lname = get('lname');
+if(typeof fname !== 'undefined' && fname !== null){
+document.getElementById('textInput').value = fname;
+//document.getElementById('textInput1').value = lname;
+}else{
+   document.getElementById('textInput').value = '';
+}
+if(typeof lname !== 'undefined' && lname !== null){
+   document.getElementById('textInput1').value = lname;
+}else{
+   document.getElementById('textInput1').value = '';
+}
+
+function printText() {
+   const textInput = document.getElementById("textInput").value;
+   const textInput1 = document.getElementById("textInput1").value;
+   const canvas = document.createElement("canvas");
+   var dwn = document.createElement('a');
+   const ctx = canvas.getContext("2d");
+   const img = new Image();
+   var dept = document.getElementById('dept').value;
+   switch (dept){
+      case "A":
+         var background = 'a.jpg';
+         var textcolor = '#7E8080';
+         var arabicfont = '200px arabic';
+         var englishfont = '200px english';
+         break;
+      case 'B':
+         var background = 'b.jpg';
+         var textcolor = '#00007A';
+         var arabicfont = '200px arabic';
+         var englishfont = '200px english';
+         break;
+      case "C":
+         var background = 'c.jpg';
+         var textcolor = '#4AC1E0';
+         var arabicfont = '200px arabicmts';
+         var englishfont = '200px englishmts';
+         break;
+      
+   }
+
+    //img.src = "bg.jpg";
+   img.src = background;
+   img.onload = function() {
+     canvas.width = img.width;
+     canvas.height = img.height;
+     ctx.drawImage(img, 0, 0);
+     ctx.fillStyle = textcolor;
+     ctx.textAlign = "center";
+     ctx.font = arabicfont;
+     ctx.fillText(textInput, canvas.width/2, (canvas.height/2)+2200);
+     ctx.font = englishfont;
+     ctx.fillText(textInput1, canvas.width/2, (canvas.height/2)+2500);
+     dwnimg = document.getElementById("image").src = canvas.toDataURL();
+     test = dwnimg;
+   }
+   document.getElementById('download').style.display = 'inline';
+}
+
+function download(){
+   printText();
+   //var downloadlink = document.createElement()
+   setTimeout(function () {
+      var dwn = document.createElement('a');
+      dwn.download = 'card.png';
+      dwn.href = test;
+      dwn.click();
+   }, 1000);
+}
